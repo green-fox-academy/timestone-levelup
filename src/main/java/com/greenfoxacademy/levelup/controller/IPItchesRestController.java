136b64c8675b9d13880c23425b0225586ea77d80
Zsolt Molnár <hulfyback@gmail.com>
2019-06-24 16:23:57 +0200

    feat(levelup): add new fields in IPitchesRestController interface
    Add new fields for:
    - headername
    - authorization status
    - error body

--- src/main/java/com/greenfoxacademy/levelup/controller/IPItchesRestController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/IPItchesRestController.java
@@ -2,4 +2,44 @@ package com.greenfoxacademy.levelup.controller;
 
 public interface IPItchesRestController {
 
+  static final String HEADER_NAME = "Authorization";
+  static final String AUTHORIZATION_STATUS_OK = "ok";
+  static final String AUTHORIZATION_STATUS_ERROR = "error";
+  static final String ERROR_BODY = "{\n"
+      + "  \"error\": \"Unauthorizied\"\n"
+      + "}".replaceAll("\\s", "");
+  static final String BODY = "{\n"
+      + "  \"myPitches\": [\n"
+      + "    {\n"
+      + "      \"timestamp\": \"2018-11-29 17:10:47\",\n"
+      + "      \"username\": \"balazs.barna\",\n"
+      + "      \"badgeName\": \"Programming\",\n"
+      + "      \"oldLevel\": 2,\n"
+      + "      \"pitchedLevel\": 3,\n"
+      + "      \"pitchMessage\": \"I improved in React, Redux, basic JS, NodeJS, Express and in LowDB, pls give me more money\",\n"
+      + "      \"holders\": [\n"
+      + "        {\n"
+      + "          \"name\": \"sandor.vass\",\n"
+      + "          \"message\": null,\n"
+      + "          \"pitchStatus\": false\n"
+      + "        },...\n"
+      + "      ],\n"
+      + "    }\n"
+      + "  ],\n"
+      + "    \"pitchesToReview\": [\n"
+      + "      {\n"
+      + "        \"timestamp\": \"2018-11-29 17:10:47\",\n"
+      + "        \"username\": \"berei.daniel\",\n"
+      + "        \"badgeName\": \"English speaker\",\n"
+      + "        \"oldLevel\": 2,\n"
+      + "        \"pitchedLevel\": 3,\n"
+      + "        \"pitchMessage\": \"I was working abroad for six years, so I can speak english very well. Pls improve my badge level to 3.\",\n"
+      + "        \"holders\": [\n"
+      + "          {\n"
+      + "            \"name\": \"balazs.jozsef\",\n"
+      + "            \"message\": \"Yes, you are able to speak english\",\n"
+      + "            \"pitchStatus\": true\n"
+      + "          },...\n"
+      + "        ]\n"
+      + "      }".replaceAll("\\s", "");
 }
