6e23a03845ac3d7299178a4b33a2c05a526452e4
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 10:46:42 +0200

    refactor(levelup): make static setAuthorized
    PitchesRestController modify isAuthorized field to static and setAuthorized() to static

--- src/main/java/com/greenfoxacademy/levelup/controller/PitchesRestController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/PitchesRestController.java
@@ -9,17 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PitchesRestController {
   private HttpHeaders responseHeader = new HttpHeaders();
-  boolean isAuthorized;
+  private static boolean isAuthorized = true;
 
-  public void setAuthorized(boolean authorized) {
+  public static void setAuthorized(boolean authorized) {
     isAuthorized = authorized;
   }
 
   @GetMapping("api/pitches")
   public ResponseEntity<String> getPitchesApiError() {
     responseHeader.set(IPItchesRestController.HEADER_NAME, "");
-
-    setAuthorized(true);
 
     if (isAuthorized == true) {
       return new ResponseEntity<String>(IPItchesRestController.BODY, responseHeader,
