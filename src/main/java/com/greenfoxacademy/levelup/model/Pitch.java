944223213afc8319a04d3c8fb5288bd0e11f49eb
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 16:37:47 +0200

    add a dummy Pitch model

---
+++ src/main/java/com/greenfoxacademy/levelup/model/Pitch.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.levelup.model;
+
+public class Pitch {
+
+  private String pitcherName;
+  private String badgeName;
+  private String newStatus;
+  private String newMessage;
+}
