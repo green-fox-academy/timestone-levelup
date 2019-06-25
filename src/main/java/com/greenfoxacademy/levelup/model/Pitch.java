3a4a82ad67ac7b2acbf8b66aad33181cc6ce1bdd
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 15:45:22 +0200

    refactor(levelup): remove default values
    Remove default values from model Pitch fields

--- src/main/java/com/greenfoxacademy/levelup/model/Pitch.java
+++ src/main/java/com/greenfoxacademy/levelup/model/Pitch.java
@@ -1,14 +1,11 @@
 package com.greenfoxacademy.levelup.model;
-
-import java.util.*;
 import java.util.List;
 
 public class Pitch {
 
-  private String badgeName = "English speaker";
-  private int oldLVL = 2;
-  private int pitchedLVL = 3;
-  private String getBadgeNamepitchMessage = "Hello World! My English is bloody gorgeous.";
-  private List<String> holders = new ArrayList(
-      Arrays.asList("balazs.jozsef", "benedek.vamosi", "balazs.barna"));
+  private String badgeName;
+  private int oldLVL;
+  private int pitchedLVL;
+  private String getBadgeNamepitchMessage;
+  private List<String> holders;
 }
