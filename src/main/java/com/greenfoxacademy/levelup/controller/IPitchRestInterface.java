cda3e571b1924aa61447751257d006a0ff4f3fc7
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 14:41:03 +0200

    feat(levelup): add constant to IPitchRestContoller
    Add new static final String for required body for using it in tests

--- src/main/java/com/greenfoxacademy/levelup/controller/IPitchRestInterface.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/IPitchRestInterface.java
@@ -2,15 +2,23 @@ package com.greenfoxacademy.levelup.controller;
 
 public interface IPitchRestInterface {
 
+  static final String PITCH_REQUIRED_BODY = "{\n"
+      + "  \"badgeName\": \"English speaker\",\n"
+      + "  \"oldLVL\": 2,\n"
+      + "  \"pitchedLVL\": 3,\n"
+      + "  \"pitchMessage\": \"Hello World! My English is bloody gorgeous.\",\n"
+      + "  \"holders\": [\"balazs.jozsef\", \"benedek.vamosi\", \"balazs.barna\"]\n"
+      + "}".replaceAll("\\s", "");
+
   static final String PITCH_SUCCESSFUL_BODY = "{\n"
       + "  \"message\": \"Created\"\n"
-      + "}";
+      + "}".replaceAll("\\s", "");
 
   static final String PITCH_UNAUTHORIZED_BODY = "{\n"
       + "  \"error\": \"Unauthorizied\"\n"
-      + "}";
+      + "}".replaceAll("\\s", "");
 
   static final String PITCH_UNSUCCESFUL_BODY = "{\n"
       + "  \"error\": \"Please provide all fields\"\n"
-      + "}";
+      + "}".replaceAll("\\s", "");
 }
