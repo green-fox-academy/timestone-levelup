f97f3805d3056beab53330b0400e5d71a541f4db
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 14:15:30 +0200

    refactor(levelup): remove required body constant

--- src/main/java/com/greenfoxacademy/levelup/controller/IPitchRestInterface.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/IPitchRestInterface.java
@@ -2,14 +2,6 @@ package com.greenfoxacademy.levelup.controller;
 
 public interface IPitchRestInterface {
 
-  static final String PITCH_REQUIRED_BODY = "{\n"
-      + "  \"badgeName\": \"English speaker\",\n"
-      + "  \"oldLVL\": 2,\n"
-      + "  \"pitchedLVL\": 3,\n"
-      + "  \"pitchMessage\": \"Hello World! My English is bloody gorgeous.\",\n"
-      + "  \"holders\": [\"balazs.jozsef\", \"benedek.vamosi\", \"balazs.barna\"]\n"
-      + "}";
-
   static final String PITCH_SUCCESSFUL_BODY = "{\n"
       + "  \"message\": \"Created\"\n"
       + "}";
