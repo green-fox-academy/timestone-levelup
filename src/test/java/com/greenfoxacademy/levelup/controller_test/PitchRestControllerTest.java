ee8a9a1e92eb06bdd1de314fb821091cdc83e9bc
Zsolt Molnár <hulfyback@gmail.com>
2019-06-25 14:42:52 +0200

    test(levelup): test authorization succesful
    This test checks if authorization is succesful then response status is 201

---
+++ src/test/java/com/greenfoxacademy/levelup/controller_test/PitchRestControllerTest.java
@@ -0,0 +1,44 @@
+package com.greenfoxacademy.levelup.controller_test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.greenfoxacademy.levelup.controller.IPitchRestInterface;
+import com.greenfoxacademy.levelup.controller.PitchRestController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class PitchRestControllerTest {
+  @Mock
+  PitchRestController pitchRestController;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void whenAuthorizationIsOk_thenReturnsStatusCode201() throws Exception {
+    mockMvc.perform(post("/api/pitch")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
+        .andExpect(status().isCreated())
+        .andDo(print());
+  }
+
+  
+}
