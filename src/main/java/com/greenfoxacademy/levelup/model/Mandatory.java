0f3e7d6ec827e2d0b18601982ac3ab2114fe9589
Zsolt Molnár <hulfyback@gmail.com>
2019-06-26 11:46:42 +0200

    feat(levelup): set fields in Mandatory
    Add setters and a new constructor to Mandatory class

--- src/main/java/com/greenfoxacademy/levelup/model/Mandatory.java
+++ src/main/java/com/greenfoxacademy/levelup/model/Mandatory.java
@@ -1,9 +1,48 @@
 package com.greenfoxacademy.levelup.model;
 
+import java.lang.reflect.Field;
+
 public class Mandatory {
 
   private String pitcherName;
   private String badgeName;
   private String newStatus;
   private String newMessage;
+
+  public boolean hasNullField() throws IllegalAccessException {
+    for (Field field: this.getClass().getDeclaredFields()) {
+      field.setAccessible(true);
+      if (field.get(this) == null) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public Mandatory() {
+  }
+
+  public Mandatory(String pitcherName, String badgeName, String newStatus,
+      String newMessage) {
+    this.pitcherName = pitcherName;
+    this.badgeName = badgeName;
+    this.newStatus = newStatus;
+    this.newMessage = newMessage;
+  }
+
+  public void setPitcherName(String pitcherName) {
+    this.pitcherName = pitcherName;
+  }
+
+  public void setBadgeName(String badgeName) {
+    this.badgeName = badgeName;
+  }
+
+  public void setNewStatus(String newStatus) {
+    this.newStatus = newStatus;
+  }
+
+  public void setNewMessage(String newMessage) {
+    this.newMessage = newMessage;
+  }
 }
