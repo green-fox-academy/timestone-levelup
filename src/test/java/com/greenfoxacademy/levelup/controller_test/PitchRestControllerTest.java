5697066340f29c4cbc327c7e4fdf8a9e2d05f245
Zsolt Molnár <hulfyback@gmail.com>
2019-06-26 18:30:49 +0200

    fix(levelup): call header() in tests
    To check authorization in tests, header() method is called

--- src/test/java/com/greenfoxacademy/levelup/controller_test/PitchRestControllerTest.java
+++ src/test/java/com/greenfoxacademy/levelup/controller_test/PitchRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.levelup.controller_test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.greenfoxacademy.levelup.controller.IPitchRestController;
@@ -20,54 +21,40 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class PitchRestControllerTest {
-  private Mandatory mandatory = new Mandatory();
-
-  @Mock
-  PitchRestController pitchRestController;
-
   @Autowired
   MockMvc mockMvc;
 
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
-    mandatory.setBadgeName("badge");
-    mandatory.setNewMessage("message");
-    mandatory.setPitcherName("pitcher name");
-    mandatory.setNewStatus("status");
   }
 
   @Test
   public void whenAuthorizationIsOkAndHasNoMissingField_thenReturnsStatusCode201() throws Exception {
-    pitchRestController
-        .setHeader(IPitchRestController.AUTHORIZATION, IPitchRestController.AUTHORIZATION_VALUES[0]);
 
     mockMvc.perform(put("/api/pitch")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(IPitchRestController.PITCH_REQUIRED_BODY))
-        .andExpect(status().isOk());
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(IPitchRestController.PITCH_REQUIRED_BODY)
+      .header("Authorization", "Full"))
+      .andExpect(jsonPath("$.pitcherName").value(""))
+      .andExpect(status().isOk());
   }
 
   @Test
   public void whenAuthorizationIsOkAndHasMissingField_thenReturnsStatusCode404() throws Exception {
-    pitchRestController
-        .setHeader(IPitchRestController.AUTHORIZATION, IPitchRestController.AUTHORIZATION_VALUES[0]);
-    mandatory.setNewStatus(null);
-
     mockMvc.perform(put("/api/pitch")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(IPitchRestController.PITCH_REQUIRED_BODY))
-        .andExpect(status().isNotFound());
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(IPitchRestController.PITCH_REQUIRED_BODY)
+      .header("Authorization", "Full"))
+      .andExpect(status().isNotFound());
   }
 
   @Test
   public void whenAuthorizationIsUnsuccessful_thenReturnsStatusCode401() throws Exception {
-    pitchRestController
-        .setHeader(IPitchRestController.AUTHORIZATION, IPitchRestController.AUTHORIZATION_VALUES[1]);
-
     mockMvc.perform(put("/api/pitch")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(IPitchRestController.PITCH_REQUIRED_BODY))
-        .andExpect(status().is4xxClientError());
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(IPitchRestController.PITCH_REQUIRED_BODY)
+      .header("Authorization", "Deny"))
+      .andExpect(status().is4xxClientError());
   }
 }
