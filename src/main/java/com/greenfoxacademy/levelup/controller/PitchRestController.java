46ce47776296dc16a42efd1fbdbe65ce614883c1
Zsolt Molnár <hulfyback@gmail.com>
2019-06-26 18:24:58 +0200

    refactor(levelup): use @RequestParam
    Intead of calling HttpHeaders.set() method to set 'Authorization' in header, add @RequestParam to putPitchApi method
    Remove unused setAuthorization() method
    Remove unused isAuthorized field

--- src/main/java/com/greenfoxacademy/levelup/controller/PitchRestController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/PitchRestController.java
@@ -1,35 +1,32 @@
 package com.greenfoxacademy.levelup.controller;
 
 import com.greenfoxacademy.levelup.model.Mandatory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PitchRestController {
-  private static HttpHeaders httpHeaders = new HttpHeaders();
 
-  public void setHeader(String key, String value) {
-    httpHeaders.set(key, value);
+  private boolean hasMissingField (Mandatory mandatory) throws Exception {
+    return mandatory.hasNullField();
   }
 
   @PutMapping("/api/pitch")
-  public ResponseEntity<String> putPitchApi(@RequestBody Mandatory mandatory) {
-    setHeader(IPitchRestController.AUTHORIZATION, IPitchRestController.AUTH_OK);
+  public ResponseEntity<String> putPitchApi(@RequestHeader(value="Authorization") String authorization,
+      @RequestBody Mandatory mandatory) throws  Exception {
 
-    if (httpHeaders.getValuesAsList(IPitchRestController.AUTHORIZATION).get(0)
-        .equals(IPitchRestController.AUTH_OK)) {
-      return new ResponseEntity<String>(IPitchRestController.PITCH_SUCCESSFUL_BODY, httpHeaders,
+    if (authorization.equals("Full") && hasMissingField(mandatory)) {
+      return new ResponseEntity<>(IPitchRestController.PITCH_UNSUCCESFUL_BODY,
+          HttpStatus.NOT_FOUND);
+    }else if (authorization.equals("Full")) {
+      return new ResponseEntity<>(IPitchRestController.PITCH_SUCCESSFUL_BODY,
           HttpStatus.OK);
-    } else if (httpHeaders.getValuesAsList(IPitchRestController.AUTHORIZATION).get(0)
-        .equals(IPitchRestController.AUTH_UNAUTHORIZED)) {
-      return new ResponseEntity<String>(IPitchRestController.PITCH_UNAUTHORIZED_BODY, httpHeaders,
-          HttpStatus.UNAUTHORIZED);
     }
-    return new ResponseEntity<String>(IPitchRestController.PITCH_UNSUCCESFUL_BODY, httpHeaders,
-        HttpStatus.NOT_FOUND);
-  }
+    return new ResponseEntity<>(IPitchRestController.PITCH_UNAUTHORIZED_BODY,
+        HttpStatus.UNAUTHORIZED);
+    }
 }
