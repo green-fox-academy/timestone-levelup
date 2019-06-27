1e6ca3ba6ad3a92750486cb8f99374eac6978a75
Zsolt Molnár <hulfyback@gmail.com>
2019-06-27 10:50:53 +0200

    feat(levelup): get mandatory fields

--- src/main/java/com/greenfoxacademy/levelup/model/Mandatory.java
+++ src/main/java/com/greenfoxacademy/levelup/model/Mandatory.java
@@ -30,16 +30,32 @@ public class Mandatory {
     this.newMessage = newMessage;
   }
 
+  public String getPitcherName() {
+    return pitcherName;
+  }
+
   public void setPitcherName(String pitcherName) {
     this.pitcherName = pitcherName;
+  }
+
+  public String getBadgeName() {
+    return badgeName;
   }
 
   public void setBadgeName(String badgeName) {
     this.badgeName = badgeName;
   }
 
+  public String getNewStatus() {
+    return newStatus;
+  }
+
   public void setNewStatus(String newStatus) {
     this.newStatus = newStatus;
+  }
+
+  public String getNewMessage() {
+    return newMessage;
   }
 
   public void setNewMessage(String newMessage) {
