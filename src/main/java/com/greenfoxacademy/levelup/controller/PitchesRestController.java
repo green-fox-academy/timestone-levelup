39227ba517f122131ee5d93b065caf5016177bd4
Zsolt Molnár <hulfyback@gmail.com>
2019-06-27 07:14:22 +0200

    refactor(levelup): remove unused methods()
    Remove IsAuthorized and httpHeaders fields
    Remove setAuthorization() and httpHeaders.set() methods
    Add real logic to check authorization

--- src/main/java/com/greenfoxacademy/levelup/controller/PitchesRestController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/PitchesRestController.java
@@ -1,29 +1,22 @@
 package com.greenfoxacademy.levelup.controller;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PitchesRestController {
-  private HttpHeaders responseHeader = new HttpHeaders();
-  private static boolean isAuthorized = true;
-
-  public static void setAuthorized(boolean authorized) {
-    isAuthorized = authorized;
-  }
 
   @GetMapping("api/pitches")
-  public ResponseEntity<String> getPitchesApi() {
-    responseHeader.set(IPItchesRestController.HEADER_NAME, "");
+  public ResponseEntity<String> getPitchesApi(@RequestHeader("Authorization") String authorization) {
 
-    if (isAuthorized) {
-      return new ResponseEntity<String>(IPItchesRestController.BODY, responseHeader,
+    if (authorization.equals("Full")) {
+      return new ResponseEntity<String>(IPItchesRestController.BODY,
           HttpStatus.OK);
     }
-    return new ResponseEntity<String>(IPItchesRestController.ERROR_BODY, responseHeader,
+    return new ResponseEntity<String>(IPItchesRestController.ERROR_BODY,
         HttpStatus.UNAUTHORIZED);
   }
 }
