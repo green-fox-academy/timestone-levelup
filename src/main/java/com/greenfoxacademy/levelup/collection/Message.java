04bab1948ff32ba0f35e12f21d2e45e8ffc3c5e9
Zsolt Molnár <hulfyback@gmail.com>
2019-06-28 14:19:13 +0200

    fix(levelup): fix PITCH_REQUIRED_BODY
    In collection.Message class:
    ---------------------------
    PITCH_REQUIRED_BODY: remove unnecessary '{}' and 'mandatory fields'
    delete unused constants
    add new constants

--- src/main/java/com/greenfoxacademy/levelup/collection/Message.java
+++ src/main/java/com/greenfoxacademy/levelup/collection/Message.java
@@ -2,7 +2,10 @@ package com.greenfoxacademy.levelup.collection;
 
 public class Message {
   public static final String AUTHORIZATION = "Authorization";
-  public static final String[] AUTHORIZATION_VALUES = new String[]{"Full", "Denied"};
+  public static final String AUTHORIZATION_OK = "Ok";
+  public static final String AUTHORIZATION_DENIED = "Denied";
+
+
 
   public static final String PITCH_SUCCESSFUL_BODY = "{\n"
       + "  \"message\": \"Success\"\n"
@@ -16,13 +19,10 @@ public class Message {
       + "  \"error\": \"Please provide all fields\"\n"
       + "}".replaceAll("\\s", "");
 
-  public static final String PITCH_REQUIRED_BODY = "{\n"
-      + "    \"mandatory fields\":\n"
-      + "    {\n"
+  public static final String PITCH_REQUIRED_BODY = "{"
       + "    \t\"pitcherName\": \"\",\n"
       + "    \t\"badgeName\": \"english speaker\",\n"
       + "    \t\"newStatus\": \"\",\n"
       + "    \t\"newMessage\": \"\"\n"
-      + "    }\n"
-      + "}";
+      + "    }";
 }
