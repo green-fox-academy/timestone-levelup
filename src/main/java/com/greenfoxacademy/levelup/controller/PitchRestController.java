8c8a13ec8dadf23e003a581824e28cc0d2f18918
lazarsoma <lazarsoma@gmail.com>
2019-06-28 11:46:46 +0200

    fix(levelup): fixing based on the review
    Fixing the model(setters and getters), the controller(changing the
    logic, deleting unused methods) and the test(fixing whenMissingField_thenReturnsStatusCode404 test)

--- src/main/java/com/greenfoxacademy/levelup/controller/PitchRestController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/PitchRestController.java
@@ -1,39 +1,28 @@
 package com.greenfoxacademy.levelup.controller;
 
 import com.greenfoxacademy.levelup.model.Pitch;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PitchRestController {
-  HttpHeaders httpHeaders = new HttpHeaders();
-  private static boolean isAuthorized = true;
-  private static boolean isMissingField = false;
-
-  public static void setAuthorization(boolean authorized) {
-    isAuthorized = authorized;
-  }
-
-  public static void setMissingFields(boolean missingField) {
-    isMissingField = missingField;
-  }
 
   @PostMapping("/api/pitch")
-  public ResponseEntity<String> postPitchApi(@RequestBody Pitch pitch) {
-    httpHeaders.set("Authorization", "");
+  public ResponseEntity<String> postPitchApi(@RequestHeader(value = "Authorization", required = false) String authorization, @RequestBody(required = false) Pitch pitch) {
 
-    if (isAuthorized) {
-      return new ResponseEntity<String>(IPitchRestInterface.PITCH_SUCCESSFUL_BODY, httpHeaders,
-          HttpStatus.CREATED);
-    } else if (isMissingField) {
-      return new ResponseEntity<String>(IPitchRestInterface.PITCH_UNSUCCESFUL_BODY, httpHeaders,
-          HttpStatus.NOT_FOUND);
+    if (authorization == null || authorization.equals("")) {
+      return new ResponseEntity<>(IPitchRestInterface.PITCH_UNAUTHORIZED_BODY,
+              HttpStatus.UNAUTHORIZED);
+    } else if (pitch.getBadgeName() == null || pitch.getHolders() == null || pitch.getOldLVL() == 0 ||
+            pitch.getPitchedLVL() == 0 || pitch.getPitchMessage() == null) {
+      return new ResponseEntity<>(IPitchRestInterface.PITCH_UNSUCCESFUL_BODY,
+              HttpStatus.NOT_FOUND);
     }
-    return new ResponseEntity<String>(IPitchRestInterface.PITCH_UNAUTHORIZED_BODY, httpHeaders,
-        HttpStatus.UNAUTHORIZED);
+    return new ResponseEntity<>(IPitchRestInterface.PITCH_SUCCESSFUL_BODY,
+            HttpStatus.CREATED);
   }
 }
--- src/test/java/com/greenfoxacademy/levelup/controller_test/PitchRestControllerTest.java
+++ src/test/java/com/greenfoxacademy/levelup/controller_test/PitchRestControllerTest.java
@@ -1,15 +1,12 @@
 package com.greenfoxacademy.levelup.controller_test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.greenfoxacademy.levelup.controller.IPitchRestInterface;
-import com.greenfoxacademy.levelup.controller.PitchRestController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -20,8 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class PitchRestControllerTest {
-  @Mock
-  PitchRestController pitchRestController;
 
   @Autowired
   MockMvc mockMvc;
@@ -33,30 +28,36 @@ public class PitchRestControllerTest {
 
   @Test
   public void whenAuthorizationIsOk_thenReturnsStatusCode201() throws Exception {
+
     mockMvc.perform(post("/api/pitch")
-      .contentType(MediaType.APPLICATION_JSON)
-      .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
-      .andExpect(status().isCreated());
+            .header("Authorization", "author")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
+            .andExpect(status().isCreated());
   }
 
   @Test
   public void whenStatusIsUnauthorized_thenReturnsStatusCode401() throws Exception {
-    pitchRestController.setAuthorization(false);
 
     mockMvc.perform(post("/api/pitch")
-      .contentType(MediaType.APPLICATION_JSON)
-      .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
-      .andExpect(status().is4xxClientError());
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
+            .andExpect(status().is4xxClientError());
   }
 
   @Test
   public void whenMissingField_thenReturnsStatusCode404() throws Exception {
-    pitchRestController.setAuthorization(false);
-    pitchRestController.setMissingFields(true);
 
-    mockMvc.perform(post("/api/pitch")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(IPitchRestInterface.PITCH_REQUIRED_BODY))
-        .andExpect(status().isNotFound());
+    String requiredBody = "{\n"
+            + "  \"oldLVL\": 2,\n"
+            + "  \"pitchedLVL\": 3,\n"
+            + "  \"pitchMessage\": \"Hello World! My English is bloody gorgeous.\",\n"
+            + "  \"holders\": [\"balazs.jozsef\", \"benedek.vamosi\", \"balazs.barna\"]\n"
+            + "}".replaceAll("\\s", "");
+
+    mockMvc.perform(post("/api/pitch").header("Authorization", "author")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requiredBody))
+            .andExpect(status().isNotFound());
   }
 }
