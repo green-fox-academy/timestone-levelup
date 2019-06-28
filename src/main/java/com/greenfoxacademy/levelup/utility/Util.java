3137dd2a704f5e7f0dda5fc82c4133d1cd9fec50
Zsolt Molnár <hulfyback@gmail.com>
2019-06-28 16:32:32 +0200

    chore(levelup): add business logic of RestController

--- src/main/java/com/greenfoxacademy/levelup/utility/Util.java
+++ src/main/java/com/greenfoxacademy/levelup/utility/Util.java
@@ -1,5 +1,20 @@
 package com.greenfoxacademy.levelup.utility;
 
+import com.greenfoxacademy.levelup.collection.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class Util {
 
+  public static ResponseEntity<String> isAuthorized(String authorization) {
+    {
+
+      if (authorization.equals(Message.AUTHORIZATION_OK)) {
+        return new ResponseEntity<>(Message.BODY,
+            HttpStatus.OK);
+      }
+      return new ResponseEntity<>(Message.ERROR_BODY,
+          HttpStatus.UNAUTHORIZED);
+    }
+  }
 }
