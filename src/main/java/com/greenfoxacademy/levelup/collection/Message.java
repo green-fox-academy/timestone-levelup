c0f406fd4331585e90f86f6d51a0e01e70ad8cb1
Zsolt Molnar <32913441+hulfyback@users.noreply.github.com>
2019-06-29 21:04:58 +0200

    style(levelup): delete empty lines

--- src/main/java/com/greenfoxacademy/levelup/collection/Message.java
+++ src/main/java/com/greenfoxacademy/levelup/collection/Message.java
@@ -4,21 +4,15 @@ public class Message {
   public static final String AUTHORIZATION = "Authorization";
   public static final String AUTHORIZATION_OK = "Ok";
   public static final String AUTHORIZATION_DENIED = "Denied";
-
-
-
   public static final String PITCH_SUCCESSFUL_BODY = "{\n"
       + "  \"message\": \"Success\"\n"
       + "}".replaceAll("\\s", "");
-
   public static final String PITCH_UNAUTHORIZED_BODY = "{\n"
       + "  \"error\": \"Unauthorizied\"\n"
       + "}".replaceAll("\\s", "");
-
   public static final String PITCH_UNSUCCESSFUL_BODY = "{\n"
       + "  \"error\": \"Please provide all fields\"\n"
       + "}".replaceAll("\\s", "");
-
   public static final String PITCH_REQUIRED_BODY = "{"
       + "    \t\"pitcherName\": \"\",\n"
       + "    \t\"badgeName\": \"english speaker\",\n"
