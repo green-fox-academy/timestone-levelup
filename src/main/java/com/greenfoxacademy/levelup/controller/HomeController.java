857494aed982615794ce59797d30037c1beb4441
Róza <rozanagy88@gmail.com>
2019-07-03 11:19:40 +0200

    fix(levelup):correct index.html refactor(levelup):change templates folder name

--- src/main/java/com/greenfoxacademy/levelup/controller/HomeController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/HomeController.java
@@ -3,10 +3,11 @@ package com.greenfoxacademy.levelup.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
   @RequestMapping("/home")
   public String renderHomePage() {
-    return "levelUpTemplate";
+    return "index";
   }
 }
