45ca8f7185a6be985b3512cd67ee8571369c667b
Zsolt Molnár <hulfyback@gmail.com>
2019-07-05 15:29:42 +0200

    chore(levelup): add type to CrudRepository
    set up the CrudRepository types in repository interfaces

--- src/main/java/com/greenfoxacademy/levelup/repository/IBadgeLevelRepository.java
+++ src/main/java/com/greenfoxacademy/levelup/repository/IBadgeLevelRepository.java
@@ -1,9 +1,10 @@
 package com.greenfoxacademy.levelup.repository;
 
+import com.greenfoxacademy.levelup.model.BadgeLevel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBadgeLevelRepository extends CrudRepository {
+public interface IBadgeLevelRepository extends CrudRepository<BadgeLevel, Long> {
 
 }
