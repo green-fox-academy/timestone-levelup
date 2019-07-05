57e00988fe394afc4916c66d84385e4d896572e5
Zsolt Molnár <hulfyback@gmail.com>
2019-07-05 16:09:17 +0200

    fix(levelup): change Level type to Long

--- src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
+++ src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBadgeRepository extends CrudRepository<Badge, Level> {
+public interface IBadgeRepository extends CrudRepository<Badge, Long> {
 
 }
