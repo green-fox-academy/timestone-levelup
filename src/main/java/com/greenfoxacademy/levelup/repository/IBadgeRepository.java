a761479ef6dd599aa26b5dd1d4b8829a7ee542cb
Zsolt Molnár <hulfyback@gmail.com>
2019-07-05 12:30:47 +0200

    chore(levelup): rename repo interfaces
    rename all repositories starting with a letter 'I'

--- src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
+++ src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BadgeRepository extends CrudRepository {
+public interface IBadgeRepository extends CrudRepository {
 
 }
