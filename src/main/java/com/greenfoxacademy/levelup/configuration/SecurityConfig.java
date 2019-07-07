599bda771de15d8cec01018e562c3524b0075a0e
lazarsoma <lazarsoma@gmail.com>
2019-07-07 13:07:39 +0200

    feat(levelup): create Google authentication
    Creating configuration package with SecurityConfig and
    CustomAuthenticationFailureHandler.
    Creating application properties.

---
+++ src/main/java/com/greenfoxacademy/levelup/configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.levelup.configuration;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+@EnableWebSecurity
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .authorizeRequests()
+            .antMatchers("/api/**").permitAll()
+            .antMatchers("/").permitAll()
+            .and()
+            .formLogin()
+            .failureHandler(customAuthenticationFailureHandler());
+  }
+
+  @Bean
+  public AuthenticationFailureHandler customAuthenticationFailureHandler() {
+    return new CustomAuthenticationFailureHandler();
+  }
+}
