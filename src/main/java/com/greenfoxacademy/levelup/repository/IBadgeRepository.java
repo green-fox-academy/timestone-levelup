66e12a8811371be6e298bfe248677d2bc19ead1f
lazarsoma <lazarsoma@gmail.com>
2019-07-10 16:18:11 +0200

    Fix(levelup): repositories

--- src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
+++ src/main/java/com/greenfoxacademy/levelup/repository/IBadgeRepository.java
@@ -1,9 +1,10 @@
 package com.greenfoxacademy.levelup.repository;
 
+import com.greenfoxacademy.levelup.model.Badge;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBadgeRepository extends CrudRepository {
+public interface IBadgeRepository extends CrudRepository <Badge, Long> {
 
 }
