313c127252d61307206460f0113fb08e6b4fa5b0
Zsolt Molnar <zsolt.molnar@workmail.com>
2019-07-11 12:40:52 +0200

    chore(levelup): replace send slack notification util to a service - create ISlackoNotificationService interface - create SlackNotificationServiceImp class implements interface - move logic from former util class to this ServiceImp class

---
+++ src/main/java/com/greenfoxacademy/levelup/service/SlackNotificationServiceImp.java
@@ -0,0 +1,42 @@
+package com.greenfoxacademy.levelup.service;
+
+import com.greenfoxacademy.levelup.model.SlackNotification;
+import java.io.IOException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SlackNotificationServiceImp implements ISlackNotificationService {
+
+  private static String slackWebhookUrl;
+
+  public SlackNotificationServiceImp(@Value("${WEBHOOK_URL}") String slackWebhookUrl) {
+    SlackNotificationServiceImp.slackWebhookUrl = slackWebhookUrl;
+  }
+
+  @Override
+  public void sendSlackNotification(SlackNotification slackNotification) {
+    CloseableHttpClient client = HttpClients.createDefault();
+    HttpPost httpPost = new HttpPost(slackWebhookUrl);
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String json = objectMapper.writeValueAsString(slackNotification);
+
+      StringEntity entity = new StringEntity(json);
+      httpPost.setEntity(entity);
+      httpPost.setHeader("Accept", "application/json");
+      httpPost.setHeader("Content-type", "application/json");
+
+      client.execute(httpPost);
+      client.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/main/java/com/greenfoxacademy/levelup/utility/SlackNotificationUtil.java
+++ src/main/java/com/greenfoxacademy/levelup/utility/SlackNotificationUtil.java
@@ -16,22 +16,5 @@ public class SlackNotificationUtil {
   private static String slackWebhookUrl;
 
   public static void sendMessage(SlackNotification message) {
-    CloseableHttpClient client = HttpClients.createDefault();
-    HttpPost httpPost = new HttpPost(slackWebhookUrl);
-
-    try {
-      ObjectMapper objectMapper = new ObjectMapper();
-      String json = objectMapper.writeValueAsString(message);
-
-      StringEntity entity = new StringEntity(json);
-      httpPost.setEntity(entity);
-      httpPost.setHeader("Accept", "application/json");
-      httpPost.setHeader("Content-type", "application/json");
-
-      client.execute(httpPost);
-      client.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 }
