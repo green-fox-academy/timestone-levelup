75a6ad76c3ce2cc23505a064a8f3c2feaadeeb88
lazarsoma <lazarsoma@gmail.com>
2019-07-12 15:27:49 +0200

    fix(levelup): Fix SecurityConfig class

--- src/main/java/com/greenfoxacademy/levelup/configuration/SecurityConfig.java
+++ src/main/java/com/greenfoxacademy/levelup/configuration/SecurityConfig.java
@@ -4,25 +4,85 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-@EnableWebSecurity
+
 @Configuration
 @EnableOAuth2Sso
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-            .authorizeRequests()
-            .antMatchers("/api/**").permitAll()
-            .antMatchers("/").permitAll()
-            .and()
-            .formLogin()
-            .failureHandler(customAuthenticationFailureHandler());
+  public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+      http
+              .csrf()
+              .disable()
+              .antMatcher("/**")
+              .authorizeRequests()
+              .antMatchers("/api/**")
+              .permitAll()
+              .anyRequest()
+              .authenticated();
+
+      /*http.antMatcher("/**")
+              .authorizeRequests()
+              .antMatchers("/", "/api**")
+              .permitAll()
+              .anyRequest()
+              .authenticated();*/
+      /*http.authorizeRequests()
+              .antMatchers("/api**")
+              .permitAll()
+              .anyRequest()
+              .authenticated()
+              .and()
+              .oauth2Login();
+              //.loginPage("/oauth_login");*/
+      /*http
+              .authorizeRequests()
+              .antMatchers("/badgelibrary")
+              .authenticated()
+              .and()
+              .oauth2Login()
+              .and()
+              .authorizeRequests()
+              //.and()
+             // .logout().logoutUrl("/logout").logoutSuccessUrl("/");
+              .antMatchers("/", "/api/**")
+              .permitAll();*/
+    }
   }
+
+  /*@Configuration
+  @Order(1)
+  public class ApiAndHomeConfiguration extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+              .authorizeRequests()
+              .antMatchers("/api/**").permitAll()
+              .antMatchers("/").permitAll();
+    }
+  }
+
+  @Configuration
+  @Order(2)
+  @EnableOAuth2Sso
+
+  public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+              .authorizeRequests()
+              .anyRequest().authenticated()
+              .and()
+              .formLogin()
+              .failureHandler(customAuthenticationFailureHandler());
+    }
+  }*/
 
   @Bean
   public AuthenticationFailureHandler customAuthenticationFailureHandler() {
