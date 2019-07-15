3ff6a51dce3569df9fb2f8de1e33f6450d18520b
Zsolt Molnar <zsolt.molnar@workmail.com>
2019-07-15 13:39:49 +0200

    fix(levelup): move @Getter/@Setter Anotations up
    put @Getter and @Setter before class instead of fields

--- src/main/java/com/greenfoxacademy/levelup/model/Review.java
+++ src/main/java/com/greenfoxacademy/levelup/model/Review.java
@@ -13,13 +13,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Review {
 
-  @Getter
-  @Setter
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
