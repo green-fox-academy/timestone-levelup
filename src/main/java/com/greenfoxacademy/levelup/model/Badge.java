91c06c52535427ee5797211b70118583450b225f
Zsolt Molnar <zsolt.molnar@workmail.com>
2019-07-15 15:24:05 +0200

    fix(levelup): update model Badge and BadgeLevel
    replace getters/setters by annotations to be consistent compared to other models
    replace constructors by annotations to be consistent compared to other models
    replace List<Badgelevel> by Set<BadgeLevel> because it's more efficient
    replace List<Person> by Set<Person> because it's more efficient
    add detailed import list instead of *

--- src/main/java/com/greenfoxacademy/levelup/model/Badge.java
+++ src/main/java/com/greenfoxacademy/levelup/model/Badge.java
@@ -1,8 +1,22 @@
 package com.greenfoxacademy.levelup.model;
 
-import javax.persistence.*;
-import java.util.List;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Badge {
   @Id
@@ -12,73 +26,7 @@ public class Badge {
   private String name;
   private String tag;
   @OneToMany(fetch = FetchType.LAZY, mappedBy = "badge")
-  private List<BadgeLevel> levels;
+  private Set<BadgeLevel> levels;
   @ManyToOne
   private Person person;
-
-  public Badge() {
-  }
-
-  public Badge(long id, String version, String name, String tag, List<BadgeLevel> levels) {
-    this.id = id;
-    this.version = version;
-    this.name = name;
-    this.tag = tag;
-    this.levels = levels;
-  }
-
-  public Badge(String version, String name, String tag, List<BadgeLevel> levels) {
-    this.version = version;
-    this.name = name;
-    this.tag = tag;
-    this.levels = levels;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getVersion() {
-    return version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getTag() {
-    return tag;
-  }
-
-  public void setTag(String tag) {
-    this.tag = tag;
-  }
-
-  public List<BadgeLevel> getLevels() {
-    return levels;
-  }
-
-  public void setLevels(List<BadgeLevel> levels) {
-    this.levels = levels;
-  }
-
-  public Person getPerson() {
-    return person;
-  }
-
-  public void setPerson(Person person) {
-    this.person = person;
-  }
 }
--- src/main/java/com/greenfoxacademy/levelup/model/BadgeLevel.java
+++ src/main/java/com/greenfoxacademy/levelup/model/BadgeLevel.java
@@ -1,7 +1,22 @@
 package com.greenfoxacademy.levelup.model;
-import javax.persistence.*;
-import java.util.List;
 
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class BadgeLevel {
   @Id
@@ -13,64 +28,5 @@ public class BadgeLevel {
   @ManyToOne
   private Badge badge;
   @OneToMany(fetch = FetchType.LAZY, mappedBy = "badgeLevel")
-  private List<Person> holders;
-
-  public BadgeLevel() {
-  }
-
-  public BadgeLevel(long id, int level, int weight, String description, List<Person> holders) {
-    this.id = id;
-    this.level = level;
-    this.weight = weight;
-    this.description = description;
-    this.holders = holders;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public int getLevel() {
-    return level;
-  }
-
-  public void setLevel(int level) {
-    this.level = level;
-  }
-
-  public int getWeight() {
-    return weight;
-  }
-
-  public void setWeight(int weight) {
-    this.weight = weight;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public List<Person> getHolders() {
-    return holders;
-  }
-
-  public void setHolders(List<Person> holders) {
-    this.holders = holders;
-  }
-
-  public Badge getBadge() {
-    return badge;
-  }
-
-  public void setBadge(Badge badge) {
-    this.badge = badge;
-  }
+  private Set<Person> personSet;
 }
