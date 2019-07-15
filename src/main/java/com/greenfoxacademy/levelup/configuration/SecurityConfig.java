a7888bde7cc20aacf790c6692925cfdc221b3487
lazarsoma <lazarsoma@gmail.com>
2019-07-15 12:00:06 +0200

    fix(levelup): Fix SecurityConfig class
    The problem was that there was a class inside the class.

--- src/main/java/com/greenfoxacademy/levelup/configuration/SecurityConfig.java
+++ src/main/java/com/greenfoxacademy/levelup/configuration/SecurityConfig.java
@@ -7,82 +7,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-
 @Configuration
 @EnableOAuth2Sso
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-
-      http
-              .csrf()
-              .disable()
-              .antMatcher("/**")
-              .authorizeRequests()
-              .antMatchers("/api/**")
-              .permitAll()
-              .anyRequest()
-              .authenticated();
-
-      /*http.antMatcher("/**")
-              .authorizeRequests()
-              .antMatchers("/", "/api**")
-              .permitAll()
-              .anyRequest()
-              .authenticated();*/
-      /*http.authorizeRequests()
-              .antMatchers("/api**")
-              .permitAll()
-              .anyRequest()
-              .authenticated()
-              .and()
-              .oauth2Login();
-              //.loginPage("/oauth_login");*/
-      /*http
-              .authorizeRequests()
-              .antMatchers("/badgelibrary")
-              .authenticated()
-              .and()
-              .oauth2Login()
-              .and()
-              .authorizeRequests()
-              //.and()
-             // .logout().logoutUrl("/logout").logoutSuccessUrl("/");
-              .antMatchers("/", "/api/**")
-              .permitAll();*/
-    }
+    http
+            .csrf()
+            .disable()
+            .authorizeRequests()
+            .antMatchers("/", "/api/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
   }
-
-  /*@Configuration
-  @Order(1)
-  public class ApiAndHomeConfiguration extends WebSecurityConfigurerAdapter {
-    protected void configure(HttpSecurity http) throws Exception {
-      http
-              .authorizeRequests()
-              .antMatchers("/api/**").permitAll()
-              .antMatchers("/").permitAll();
-    }
-  }
-
-  @Configuration
-  @Order(2)
-  @EnableOAuth2Sso
-
-  public class BasicConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-      http
-              .authorizeRequests()
-              .anyRequest().authenticated()
-              .and()
-              .formLogin()
-              .failureHandler(customAuthenticationFailureHandler());
-    }
-  }*/
 
   @Bean
   public AuthenticationFailureHandler customAuthenticationFailureHandler() {
