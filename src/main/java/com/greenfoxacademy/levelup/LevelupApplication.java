60601648f503e1692b5db581a74b01f7bd62474a
lazarsoma <lazarsoma@gmail.com>
2019-08-06 10:55:40 +0200

    feat(levelup): create Springfox
    Set dependencencies, annotations and method needed

--- src/main/java/com/greenfoxacademy/levelup/LevelupApplication.java
+++ src/main/java/com/greenfoxacademy/levelup/LevelupApplication.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.levelup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,9 +18,12 @@ public class LevelupApplication {
   }
 
   @Bean
-  public Docket productApi() {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-            .apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+  public Docket apiDocket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
   }
 }
 
