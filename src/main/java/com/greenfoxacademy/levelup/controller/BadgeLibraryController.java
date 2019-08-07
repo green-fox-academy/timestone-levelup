c655a2a746376f6d7f5ac36c82da2945de5c1629
Róza <rozanagy88@gmail.com>
2019-08-07 06:04:31 +0200

    feat(levelup): create endpoints for the createBadge form chore(levelup): update createBadge html

--- src/main/java/com/greenfoxacademy/levelup/controller/BadgeLibraryController.java
+++ src/main/java/com/greenfoxacademy/levelup/controller/BadgeLibraryController.java
@@ -1,9 +1,11 @@
 package com.greenfoxacademy.levelup.controller;
 
+import com.greenfoxacademy.levelup.model.Badge;
 import com.greenfoxacademy.levelup.service.IBadgeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BadgeLibraryController {
@@ -19,4 +21,15 @@ public class BadgeLibraryController {
     model.addAttribute("badges", badgeService.findAll());
     return "badgeLibrary";
   }
+
+  @GetMapping("/badgelibrary/create")
+  public String renderCreateForm() {
+    return "createBadge";
+  }
+
+  /*@PostMapping("/badgelibrary/create")
+  public String createNewBadge(String version, String name, String tag) {
+    Badge badge = new Badge(version, name, tag);
+    return "redirect:/badgelibrary";
+  }*/
 }
